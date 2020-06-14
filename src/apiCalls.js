4a0a55899b064f3f4e20f16aed0bf5d081086749
Jeremy Poulter <Jpoulter@gmail.com>
2020-06-14 11:59:22 -0600

    Create functions for creating a new url post and removing an existing one

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,24 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrl = (long_url, title) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      long_url, title
+    })
+  })
+    .then(response => response.json())
+  .catch(err => console.error(err))
+}
+
+export const removeUrl = (urlID) => {
+  return fetch(`http://localhost:3001/api/v1/urls/${urlID}`, {
+    method: 'DELETE'
+  })
+  .catch(err => console.error(err))
+}
