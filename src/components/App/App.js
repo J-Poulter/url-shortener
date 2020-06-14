27b9d85abda60abdd4baf6c86d1ecd7912e76e34
Jeremy Poulter <Jpoulter@gmail.com>
2020-06-14 12:00:09 -0600

    Create functions for adding/removing url posts to the dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,44 @@
-import React, { Component } from 'react';
-import './App.css';
-import { getUrls } from '../../apiCalls';
-import UrlContainer from '../UrlContainer/UrlContainer';
-import UrlForm from '../UrlForm/UrlForm';
+import React, { Component } from "react";
+import "./App.css";
+import { getUrls, removeUrl } from "../../apiCalls";
+import UrlContainer from "../UrlContainer/UrlContainer";
+import UrlForm from "../UrlForm/UrlForm";
 
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      urls: []
-    }
+      urls: [],
+    };
   }
 
   componentDidMount() {
+    getUrls()
+      .then((data) => this.setState({ urls: data.urls }));
+  }
+
+  addPostToDom = (newPost) => {
+    this.setState({urls: [...this.state.urls, newPost]})
+  }
+
+  deletePost = (urlID) => {
+    removeUrl(urlID)
+    this.removeFromDom(urlID)
+  }
+
+  removeFromDom = (urlID) => {
+    this.setState({ urls: this.state.urls.filter(url => url.id !== urlID)})
   }
 
   render() {
     return (
-      <main className="App">
+      <main className='App'>
         <header>
           <h1>URL Shortener</h1>
-          <UrlForm />
+          <UrlForm addPostToDom={this.addPostToDom}/>
         </header>
 
-        <UrlContainer urls={this.state.urls}/>
+        <UrlContainer urls={this.state.urls} deletePost={this.deletePost}/>
       </main>
     );
   }
